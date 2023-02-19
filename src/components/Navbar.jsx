5b12db65e7b917055b3014bf64ae2eb67f5aad9e
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-19 13:01:22 +0300

    Finish basic sidbar UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,14 @@ import {
     Badge,
     Box,
     InputBase,
+    Menu,
+    MenuItem,
     Toolbar,
     Typography,
     styled,
 } from "@mui/material";
 import FitbitRoundedIcon from "@mui/icons-material/FitbitRounded";
-import React from "react";
+import React, { useState } from "react";
 import { Mail, Notifications } from "@mui/icons-material";
 
 const StyledToolbar = styled(Toolbar)({
@@ -28,36 +30,53 @@ const Icons = styled(Box)(({ theme }) => ({
     display: "none",
     alignItems: "center",
     gap: "20px",
-    [theme.breakpoints.up("sm")]:{
-        display:"flex",
-    }
+    [theme.breakpoints.up("sm")]: {
+        display: "flex",
+    },
 }));
 
 const UserBox = styled(Box)(({ theme }) => ({
     display: "flex",
     alignItems: "center",
     gap: "20px",
-    [theme.breakpoints.up("sm")]:{
-        display:"none"
-    }
-    
+    [theme.breakpoints.up("sm")]: {
+        display: "none",
+    },
 }));
 
 const Navbar = () => {
+    const [avatarMenu, setAvatarMenu] = useState(false);
+    const [openWelcoming, setOpenWelcoming] = useState(false);
+
     return (
         <AppBar position="sticky">
             <StyledToolbar>
                 <Typography
+                    onClick={e=>setOpenWelcoming(true)}
                     variant="h6"
                     sx={{ display: { xs: "none", sm: "block" } }}
                 >
                     NABIL DEV
                 </Typography>
+                <Menu
+                open={openWelcoming}
+                onClose={(e) => setOpenWelcoming(false)}
+                anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                }}
+                transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                }}
+            >
+                <MenuItem>Hi</MenuItem>
+            </Menu>
                 <FitbitRoundedIcon
                     sx={{ display: { xs: "block", sm: "none" } }}
                 />
                 <Search>
-                    <InputBase placeholder="search..." />
+                    <InputBase placeholder="search..." sx={{width:"100%"}} />
                 </Search>
                 <Icons>
                     <Badge badgeContent={4} color="error">
@@ -69,9 +88,10 @@ const Navbar = () => {
                     <Avatar
                         sx={{ width: 35, height: 35 }}
                         src="https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png"
+                        onClick={(e) => setAvatarMenu(true)}
                     />
                 </Icons>
-                <UserBox>
+                <UserBox onClick={(e) => setAvatarMenu(true)}>
                     <Avatar
                         sx={{ width: 35, height: 35 }}
                         src="https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png"
@@ -79,6 +99,24 @@ const Navbar = () => {
                     <Typography variant="span">Nabil</Typography>
                 </UserBox>
             </StyledToolbar>
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                open={avatarMenu}
+                onClose={(e) => setAvatarMenu(false)}
+                anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                }}
+                transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                }}
+            >
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>My account</MenuItem>
+                <MenuItem>Logout</MenuItem>
+            </Menu>
         </AppBar>
     );
 };
